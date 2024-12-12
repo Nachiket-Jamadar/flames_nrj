0004ca79160f3586d3d5ea1ad83766b2c05ae571
Nachiket Jamadar <41303740+Nachiket-Jamadar@users.noreply.github.com>
2024-12-13 02:01:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='flames_nrj',
-	version='0.5',
+	version='0.6',
 	description='flames_nrj library for python robot framework practice',
 	author='Nachiket Jamadar',
 	author_email='nachiketjam@gmail.com',
