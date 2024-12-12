7d63bab21d642897b6966e1224a8ccfc59edc195
Nachiket Jamadar <41303740+Nachiket-Jamadar@users.noreply.github.com>
2024-12-12 16:40:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='flames-library',
-	version='0.2',
+	version='0.3',
 	description='flames library for python robot framework practice',
 	author='Nachiket Jamadar',
 	author_email='nachiketjam@gmail.com',
