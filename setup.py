0652ff8fc229fd65d62f3521c3db106d0ce4b255
nachiket_rj <nachiketjam@gmail.com>
2024-12-12 17:52:29 +0530

    setup file update

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-	name='flames-library',
-	version='0.3',
-	description='flames library for python robot framework practice',
+	name='flames_nrj',
+	version='0.4',
+	description='flames_nrj library for python robot framework practice',
 	author='Nachiket Jamadar',
 	author_email='nachiketjam@gmail.com',
-	packages=[],
+	platforms = 'any',
+	packages=find_packages(),
 	install_requires=[
 		'robotframework',
 	],
+	include_package_data=True
 )
